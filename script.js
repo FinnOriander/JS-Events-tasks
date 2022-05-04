0f5a074020ba9d12d7afaf7f09c0f27a9b0c077d
FinnOriander <102714329@student.swin.edu.au>
2022-05-05 01:07:45 +1000

    3 files modified, 1 deleted

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello!')
+function HelloWorld() {
+  let h1 = document.getElementsByTagName('h1')[0];
+  h1.innerHTML = 'Hello World!';
+}
+
+window.onload = function() {
+  replace();
+}
+
+function replace() {
+  console.log("replace");
+  document.getElementById("Hello").innerHTML = "Ipsum Dominum";
+  
+}
+
+
+function Changebg() {
+    var element = document.body;
+    element.classList.toggle("lime-mode");
+}
